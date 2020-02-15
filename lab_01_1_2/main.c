0373acf97c62bce080f6c5aa8d9c0a9783136c95
maxhha <devmaxhha@gmail.com>
2020-02-15 12:11:10 +0300

    Removed make files

--- lab_01_1_2/main.c
+++ lab_01_1_2/main.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
-#define __USE_MISC 1
-#include <math.h>
+
+# define M_PI 3.14159265358979323846
+
+double sin(double x)
+{
+    double s = x, p = x;
+    for(int i = 2; i <= 20; i+=2)
+    {
+        p = -p*x/i*x/(i+1);
+        s += p;
+    }
+    return s;
+}
 
 int main()
 {
+    int a, b, alpha;
+    double angle, h;
+
     setbuf(stdout, NULL);
     printf("Enter a b alpha:\n");
 
-    int a, b, alpha;
     scanf("%d %d %d", &a, &b, &alpha);
 
-    double h = fabs((b - a) / 2.0 * tan(alpha * M_PI / 180.0));
+    angle = alpha * M_PI / 180.0;
+    h = (b - a) / 2.0 * sin(angle) / sin(angle + M_PI/2.0);
 
-    printf("Square is %0.2f\n", h*(a+b)/2.0);
+    if (h < 0)
+    {
+        h = -h;
+    }
+
+    printf("Square is %d\n", (int) (h*(a+b)/2.0));
     return 0;
 }
+
