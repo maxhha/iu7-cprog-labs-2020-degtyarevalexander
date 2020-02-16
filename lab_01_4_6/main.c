4e227115f3246d6a11525432ff5845e99ca94ac4
maxhha <devmaxhha@gmail.com>
2020-02-16 10:18:19 +0300

    Changed input validation

--- lab_01_4_6/main.c
+++ lab_01_4_6/main.c
@@ -16,41 +16,11 @@ int main()
     setbuf(stdout, NULL);
     printf("Segment and point:\n");
 
-    rc = scanf("%d%c", &x1, &t);
-    if (rc != 2 || rc == EOF || t == '\n')
+    rc = scanf("%d %d %d %d %d %d%c", &x1, &y1, &x2, &y2, &xa, &ya, &t);
+    if ((rc != 7 && rc != EOF) || t != '\n')
     {
         return 1;
     }
-
-    rc = scanf("%d%c", &y1, &t); 
-    if (rc != 2 || rc == EOF || t == '\n')
-    {
-        return 1;
-    }
-    
-    rc = scanf("%d%c", &x2, &t);
-    if (rc != 2 || rc == EOF || t == '\n')
-    {
-        return 1;
-    }
-    
-    rc = scanf("%d%c", &y2, &t);
-    if (rc != 2 || rc == EOF || t == '\n')
-    {
-        return 1;
-    }
-    
-    rc = scanf("%d%c", &xa, &t);
-    if (rc != 2 || rc == EOF || t == '\n')
-    {
-        return 1;
-    }
-    
-    rc = scanf("%d%c", &ya, &t);
-    if (rc != 2 || rc == EOF)
-    {
-        return 1;
-    }    
 
     printf("%d\n", is_on_segment(x1, y1, x2, y2, xa, ya));
 
