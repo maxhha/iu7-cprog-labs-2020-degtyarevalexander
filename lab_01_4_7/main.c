d14c17fcfb03a9a0846f522f81a553e55d73414d
maxhha <devmaxhha@gmail.com>
2020-02-16 14:28:54 +0300

    change type

--- lab_01_4_7/main.c
+++ lab_01_4_7/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-double s_x(double x, double eps)
+float s_x(float x, float eps)
 {
-    double res = 1;
-    double t = -x;
-    long int n = 2;
+    float res = 0;
+    float t = 1;
+    long int n = 1;
 
-    while (fabs(t * n * (n + 1.0) / 2.0) >= eps)
+    while (fabs(t) > eps)
     {
-        res += t * n * (n + 1.0) / 2.0;
+        res += t;
 
-        t = -t * x;
-        n++;
+        t = -t / n * (n + 2) * x;
+
+        n += 1;
     }
 
     return res;
@@ -20,30 +21,30 @@ double s_x(double x, double eps)
 
 int main(void)
 {
-    double x, eps;
-    double fx, sx;
-    double abs_err, rel_err;
+    float x, eps;
+    float fx, sx;
+    float abs_err, rel_err;
 
-    printf("x, eps:\n");
+    printf("x, eps:\n    ");
 
-    if (scanf("%lf %lf", &x, &eps) == 2 && fabs(x) < 1 && eps > 0)
+    if (scanf("%f %f", &x, &eps) == 2 && fabs(x) < 1 && eps > 0)
     {
         sx = s_x(x, eps);
         fx = exp(-3 * log(1 + x));
 
-        printf("\ns(x): %.6lf \nf(x): %.6lf\n", sx, fx);
+        printf("\ns(x): %.6f \nf(x): %.6f\n", sx, fx);
 
         abs_err = fabs(fx - sx);
         rel_err = fabs(abs_err / fx);
 
-        printf("\nAbsolute error: %.6lf", abs_err);
-        printf("\nRelative error: %.6lf\n", rel_err);
+        printf("\nAbsolute error: %.6f", abs_err);
+        printf("\nRelative error: %.6f\n", rel_err);
 
         return 0;
     }
     else
     {
-        printf("nonono\n");
+        printf("Ошибка при вводе данных\n");
         return 1;
     }
 }
