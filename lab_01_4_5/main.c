43c973a1be3da92e547048ae309c9171472bce35
maxhha <devmaxhha@gmail.com>
2020-03-14 10:44:03 +0300

    Added func in lab_01_4_5

--- lab_01_4_5/main.c
+++ lab_01_4_5/main.c
@@ -3,22 +3,9 @@
 #define OK 0
 #define INPUT_ERR 1
 
-int main()
+void print_factor(int n)
 {
-    int n, i, rc;
-
-    setbuf(stdout, NULL);
-    printf("N:\n");
-
-    rc = scanf("%d", &n);
-
-    if (rc != 1 || rc == EOF || n <= 0)
-    {
-        printf("no no no\n");
-        return INPUT_ERR;
-    }
-
-    i = 2;
+    int i = 2;
     while (n != 1)
     {
         if (n % i == 0)
@@ -32,6 +19,25 @@ int main()
         }
     }
     printf("\n");
+}
 
+int main()
+{
+    int n, rc;
+
+    setbuf(stdout, NULL);
+    printf("N:\n");
+
+    rc = scanf("%d", &n);
+
+    if (rc != 1 || rc == EOF || n <= 0)
+    {
+        printf("no no no\n");
+        return INPUT_ERR;
+    }
+    
+    print_factor(n);
+ 
     return OK;
 }
+
