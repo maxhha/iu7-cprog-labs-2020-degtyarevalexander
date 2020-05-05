a8cdfb42cd662388107123a807eb2301e11f77fd
maxhha <devmaxhha@gmail.com>
2020-05-05 10:01:41 +0300

    Removed word duplicates in lab_04_0_2

--- lab_04_0_2/output.c
+++ lab_04_0_2/output.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include "output.h"
 
 void print_words_counts(const char words[][WORD_STR_MAX_SIZE + 1], const int *counts, int words_n)
 {
+    int j;
     for (int i = 0; i < words_n; i++)
     {
-        printf("%s %d\n", words[i], counts[i]);
+        for (j = 0; j < i; j++)
+            if (strcmp(words[i], words[j]) == 0)
+                break;
+
+        if (j == i)
+            printf("%s %d\n", words[i], counts[i]);
     }
 }
