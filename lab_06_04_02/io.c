9e00003b42899f9ef21cd199f90f6461fd2aeed1
maxhha <devmaxhha@gmail.com>
2020-09-06 11:03:22 +0300

    fix style

--- lab_06_04_02/io.c
+++ lab_06_04_02/io.c
@@ -7,8 +7,7 @@ int read_objects_from_file(FILE *f, t_object *objects, int *n)
         char *s = objects->name;
         char c;
 
-        while (
-            fscanf(f, "%c", &c) == 1 &&
+        while (fscanf(f, "%c", &c) == 1 &&
             c != '\n' &&
             s < objects->name + MAX_NAME_LEN)
             *s++ = c;
@@ -33,15 +32,10 @@ int read_objects_from_file(FILE *f, t_object *objects, int *n)
     return fgetc(f) == EOF ? OK : ERR;
 }
 
-void print_objects(const t_object *objects, const int n)
+void print_objects(const t_object *objs, const int n)
 {
-    for (int i = 0; i < n; i++, objects++)
+    for (int i = 0; i < n; i++, objs++)
     {
-        printf(
-            "%s %0.6lf %0.6lf\n",
-            objects->name,
-            objects->weight,
-            objects->volume
-        );
+        printf("%s %0.6lf %0.6lf\n", objs->name, objs->weight, objs->volume);
     }
 }
