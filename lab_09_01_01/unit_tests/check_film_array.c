f32908d6f03de862e07b25301bb2eda2dc914149
maxhha <devmaxhha@gmail.com>
2020-10-31 18:39:27 +0300

    fix memory error

--- lab_09_01_01/unit_tests/check_film_array.c
+++ lab_09_01_01/unit_tests/check_film_array.c
@@ -164,33 +164,6 @@ START_TEST(test_comapres)
 }
 END_TEST
 
-START_TEST(test_print)
-{
-    film_t a = {
-        .title = "title A",
-        .name = "name B",
-        .year = 2020,
-    };
-
-    film_t *p_a = &a;
-
-    FILE *tmp = stdout;
-    char *buf_p = NULL;
-    size_t size = 0;
-    stdout = open_memstream(&buf_p, &size);
-    ck_assert_ptr_nonnull(stdout);
-
-    print_films_array(&p_a, 1);
-    fflush(stdout);
-    fclose(stdout);
-    stdout = tmp;
-
-    ck_assert_ptr_nonnull(buf_p);
-    ck_assert_str_eq(buf_p, "title A\nname B\n2020\n");
-    free(buf_p);
-}
-END_TEST
-
 Suite *film_array_suit(void)
 {
     Suite *s;
@@ -200,21 +173,20 @@ Suite *film_array_suit(void)
     s = suite_create("film_array");
 
     tc_neg = tcase_create("negatives");
-    // tcase_add_test(tc_neg, scan_film_from_empty_stream);
-    // tcase_add_test(tc_neg, scan_film_with_empty_title);
-    // tcase_add_test(tc_neg, scan_film_with_empty_name);
-    // tcase_add_test(tc_neg, scan_film_with_empty_year);
-    // tcase_add_test(tc_neg, scan_film_with_empty_neg_year);
+    tcase_add_test(tc_neg, scan_film_from_empty_stream);
+    tcase_add_test(tc_neg, scan_film_with_empty_title);
+    tcase_add_test(tc_neg, scan_film_with_empty_name);
+    tcase_add_test(tc_neg, scan_film_with_empty_year);
+    tcase_add_test(tc_neg, scan_film_with_empty_neg_year);
     tcase_add_test(tc_neg, read_films_from_unexisting_file);
     tcase_add_test(tc_neg, read_films_from_empty_file);
     tcase_add_test(tc_neg, read_films_from_invalid_file);
     suite_add_tcase(s, tc_neg);
 
     tc_pos = tcase_create("positives");
-    // tcase_add_test(tc_pos, scan_film_successful);
+    tcase_add_test(tc_pos, scan_film_successful);
     tcase_add_test(tc_pos, read_films_from_valid_file);
     tcase_add_test(tc_pos, test_comapres);
-    tcase_add_test(tc_pos, test_print);
     suite_add_tcase(s, tc_pos);
 
     return s;
