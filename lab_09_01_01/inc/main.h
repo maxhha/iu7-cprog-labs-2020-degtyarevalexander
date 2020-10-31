753981183512624e256507e0dbc4e91475dfa73e
maxhha <devmaxhha@gmail.com>
2020-10-31 12:01:26 +0300

    add solve for lab_09

---
+++ lab_09_01_01/inc/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "log.h"
+#include "film_array.h"
+
+#define ARG_FIELD_TITLE "title"
+#define ARG_FIELD_NAME "name"
+#define ARG_FIELD_YEAR "year"
+
+#endif // __MAIN_H__
